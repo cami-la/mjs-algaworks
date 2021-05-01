f346d5a3c79f34782089719e5547cea79551300b
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-05-01 06:26:22 -0300

    02.06-condicionais

---
+++ 02.06-condicionais/index.js
@@ -0,0 +1,14 @@
+var me = {
+  name: "Daniel",
+  age: "18"
+};
+
+function checkAge (person) {
+  
+  if (person.age === "18") {
+    console.log("Caiu dentro do bloco");
+  }
+  
+};
+
+checkAge(me);
